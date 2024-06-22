42dbb86fbf34871294584d8cde091968438c5e7c
David Jackson <dwjackson@users.noreply.github.com>
2024-06-22 11:30:32 -0400

    Implement reading map from stdin

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,57 @@
  * Copyright (c) 2024 David Jackson
  */
 
+extern crate getopts;
+use getopts::Options;
 use std::env;
+use std::io;
+use std::io::Read;
 use ttmap::compiler::compile_svg;
 use ttmap::files::read_file;
 
 const DEFAULT_DIMENSION: usize = 10;
 
+// Options
+const OPT_FILE: &str = "f";
+const OPT_DIMENSION: &str = "d";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        println!("USAGE: ttmap [MAP_FILE] [DIMENSION?]");
-        std::process::exit(1);
-    }
+    let mut opts = Options::new();
+    opts.optopt(OPT_FILE, "file", "input map file", "MAP_FILE");
+    opts.optopt(
+        OPT_DIMENSION,
+        "dimension",
+        "map cell dimension in pixels",
+        "DIMENSION",
+    );
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!("{}", e.to_string()),
+    };
 
-    let file_name = &args[1];
+    let input = if matches.opt_present(OPT_FILE) {
+        let file_name = matches.opt_str(OPT_FILE).unwrap();
+        read_file(&file_name)
+    } else {
+        let mut s = String::new();
+        io::stdin()
+            .read_to_string(&mut s)
+            .expect("Could not read stdin");
+        s
+    };
 
-    let dim = if args.len() > 2 {
-        args[2].parse::<usize>().expect("Invalid dimension")
+    let dim = if matches.opt_present(OPT_DIMENSION) {
+        matches
+            .opt_str(OPT_DIMENSION)
+            .unwrap()
+            .parse::<usize>()
+            .expect("Invalid dimension")
     } else {
         DEFAULT_DIMENSION
     };
 
-    let input = read_file(file_name);
     let s = compile_svg(&input, dim);
     println!("{}", s);
 }
